ea28d321dbbe04df7d8f62191994cfd29da9c608
deyar4 <deyarbakr4@gmail.com>
2023-03-14 09:12:13 +0300

    made the script run indefinetly and added a timer so it can do 3 commits a day

--- commit_bot.py
+++ commit_bot.py
@@ -8,31 +8,30 @@ REPO_NAME = "deyar4/Contribution-bot"
 GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
 DAD_JOKE_API_URL = "https://icanhazdadjoke.com/"
 
-# Get the current date
-now = datetime.datetime.now()
+while True:
+    time.sleep(8 * 60 * 60 + random.randint(-60, 60))
+    # Fetch a random dad joke from the API
+    headers = {"Accept": "application/json"}
+    response = requests.get(DAD_JOKE_API_URL, headers=headers)
 
-# Fetch a random dad joke from the API
-headers = {"Accept": "application/json"}
-response = requests.get(DAD_JOKE_API_URL, headers=headers)
+    if response.status_code == 200:
+        joke = response.json()["joke"]
+    else:
+        joke = "Why couldn't the bot commit the joke? Because it was a bit buggy."
 
-if response.status_code == 200:
-    joke = response.json()["joke"]
-else:
-    joke = "Why couldn't the bot commit the joke? Because it was a bit buggy."
+    # Define the commit payload
+    payload = {
+        "message": f"I am the bot that updated Dyars profile",
+        "content": base64.b64encode(joke.encode("utf-8")).decode("utf-8")
+    }
 
-# Define the commit payload
-payload = {
-    "message": f"I am the bot that updated Dyars profile",
-    "content": base64.b64encode(joke.encode("utf-8")).decode("utf-8")
-}
+    # Make the commit request
+    url = f"https://api.github.com/repos/{REPO_NAME}/contents/dad_joke.txt"
+    headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
+    response = requests.put(url, json=payload, headers=headers)
 
-# Make the commit request
-url = f"https://api.github.com/repos/{REPO_NAME}/contents/dad_joke.txt"
-headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
-response = requests.put(url, json=payload, headers=headers)
-
-if response.status_code == 200:
-    print(f"Committed successfully! Day {now.strftime('%d')} of the streak.")
-else:
-    print(f"Failed to commit.")
+    if response.status_code == 200:
+        print(f"Committed successfully! Day {now.strftime('%d')} of the streak.")
+    else:
+        print(f"Failed to commit.")
 
